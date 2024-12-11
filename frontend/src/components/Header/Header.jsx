4078c3a8c521ed32423d1fb4e694fa520f2ed040
elidrissi <elidrissilaoukili@gmail.com>
2024-12-11 12:35:48 +0100

    .

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
     <nav className="bg-gray-800 text-white shadow-md">
       <div className="flex justify-between items-center py-4 container mx-auto px-4">
         <div className="text-2xl font-bold">
-          <strong>tect</strong>
+          <strong>Elidrissi - Spring CRUD</strong>
         </div>
 
         <div className="flex space-x-4 ml-auto">
@@ -14,10 +14,7 @@ const Header = () => {
             Create Employee
           </Link>
           <Link to="/" className="m-2 px-4 py-2 rounded-lg hover:bg-gray-700">
-            Employee
-          </Link>
-          <Link to="/employee" className="m-2 px-4 py-2 rounded-lg hover:bg-gray-700">
-            Save Employee
+            Employees
           </Link>
         </div>
       </div>
